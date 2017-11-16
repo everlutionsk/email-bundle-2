c9edc23600c5aa624b2155a420462305886ac899
Martin Adamik <martin.adamik@everlution.sk>
2017-11-16 15:40:08 +0100

    resend

--- Entity/Repository/StorableOutboundMessageStatus.php
+++ Entity/Repository/StorableOutboundMessageStatus.php
@@ -6,6 +6,7 @@ use Everlution\EmailBundle\Entity\StorableOutboundMessageStatus as Entity;
 
 class StorableOutboundMessageStatus extends BaseRepository
 {
+    protected const MAX_RESEND_ATTEMPTS = 3;
 
     /**
      * @param string $mailSystemMessageId
@@ -34,7 +35,10 @@ class StorableOutboundMessageStatus extends BaseRepository
     {
         return $this
             ->createQueryBuilder('i')
+            ->join('i.storableOutboundMessage', 'm')
             ->where('i.rejectReason IS NOT NULL')
+            ->andWhere('m.resendAttempts < :resendAttempts')
+            ->setParameter('resendAttempts', self::MAX_RESEND_ATTEMPTS)
             ->getQuery()
             ->getResult();
     }
